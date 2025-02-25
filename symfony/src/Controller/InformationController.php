7f363dd55db7ddf0c2eb740371e48d025eeff963
ArthurDesc <arthur.descourvieres@gmail.com>
2025-02-25 22:53:44 +0100

    Add Bootstrap integration and create Information page
    - Updated base template to include Bootstrap CSS and JS.
    - Enhanced home page layout with Bootstrap components.
    - Added InformationController and corresponding template for information display.

---
+++ symfony/src/Controller/InformationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InformationController extends AbstractController
+{
+    #[Route('/information', name: 'app_information')]
+    public function index(): Response
+    {
+        return $this->render('information/index.html.twig', [
+            'controller_name' => 'InformationController',
+        ]);
+    }
+} 
