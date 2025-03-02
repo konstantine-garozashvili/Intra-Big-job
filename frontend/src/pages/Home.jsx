b0d39596906e201d9efd079a1f60501369e58a51
camara_aicha <aicha.ca@outlook.fr>
2025-03-02 14:02:08 +0100

    Ajout d'un fond et aggrandissement des boutons

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Navbar2 from '../components/Navbar2';
+import Footer from '../components/Footer';
 
 import axios from 'axios';
 import { Card, CardContent } from '@/components/ui/card';
@@ -14,21 +15,28 @@ const Home = () => {
     
 
   return (
-    <div className="">
-        <Navbar2 />
-   <div className="flex space-x-4 mt-6  item-center justify-center ">
-      {/* Bouton Connexion */}
-      <button className="px-6 py-3 bg-[#DEC2E8] text-[#2D055B] font-semibold rounded-lg hover:bg-blue-700 transition">
-        Connexion
-      </button>
+    <div className="h-screen flex flex-col">
+      <Navbar2 />
+      
+      {/* Contenu principal qui prend toute la hauteur restante */}
+      <main className="flex-1 flex flex-col items-center bg-[url('/src/assets/images/purplebg.jpg')]  bg-center">
+  <section className="w-full flex justify-center items-start mt-10 space-x-4">
+    {/* Bouton Connexion */}
+    <button className="w-40 px-6 py-3 bg-[#DEC2E8] text-[#2D055B] font-semibold rounded-lg hover:bg-white transition ">
+      Connexion
+    </button>
 
-      {/* Bouton Inscription */}
-      <button className="px-6 py-3 bg-[#DEC2E8] text-[#2D055B] font-semibold rounded-lg hover:bg-green-700 transition">
-        Inscription
-      </button>
-    </div>
-    </div>
-  );
-};
+    {/* Bouton Inscription */}
+    <button className="w-40 px-6 py-3 bg-[#DEC2E8] text-[#2D055B] font-semibold rounded-lg hover:bg-white transition">
+      Inscription
+    </button>
+  </section>
+</main>
 
+
+  
+      <Footer />
+    </div>
+  )};
+  
 export default Home;
