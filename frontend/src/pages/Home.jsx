40750e040d931883b3f055400a357d5d097a341c
camara_aicha <aicha.ca@outlook.fr>
2025-03-03 09:49:28 +0100

    Changement de la page entiere

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,42 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import Navbar2 from '../components/Navbar2';
-import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import { Link } from "react-router-dom"
 
-import axios from 'axios';
-import { Card, CardContent } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-
-const links = [
-    { name: 'Devenir partenaire', href: '#' },
-    { name: 'Notre mission', href: '#' },
-  ]
- 
 const Home = () => {
-    
 
   return (
     <div className="h-screen flex flex-col">
-      <Navbar2 />
-      
+      <Navbar />
+
       {/* Contenu principal qui prend toute la hauteur restante */}
-      <main className="flex-1 flex flex-col items-center bg-[url('/src/assets/images/purplebg.jpg')]  bg-center">
-  <section className="w-full flex justify-center items-start mt-10 space-x-4">
-    {/* Bouton Connexion */}
-    <button className="w-40 px-6 py-3 bg-[#DEC2E8] text-[#2D055B] font-semibold rounded-lg hover:bg-white transition ">
-      Connexion
-    </button>
+      <main className="flex-1 flex flex-col items-center bg-[url('/src/assets/images/purplebg.jpg')] bg-repeat bg-center">
+        <section className="w-full flex justify-center items-start mt-10 space-x-4">
+          {/* Bouton Connexion */}
+          <Link to="/connexion">
+            <button className="w-40 px-6 py-3 bg-[#DEC2E8] text-[#2D055B] font-semibold rounded-lg hover:bg-white transition">
+              Connexion
+            </button>
+          </Link>
 
-    {/* Bouton Inscription */}
-    <button className="w-40 px-6 py-3 bg-[#DEC2E8] text-[#2D055B] font-semibold rounded-lg hover:bg-white transition">
-      Inscription
-    </button>
-  </section>
-</main>
+          {/* Bouton Inscription */}
+          <Link to="/inscription">
+            <button className="w-40 px-6 py-3 bg-[#DEC2E8] text-[#2D055B] font-semibold rounded-lg hover:bg-white transition">
+              Inscription
+            </button>
+          </Link>
+        </section>
+      </main>
 
-
-  
-      <Footer />
     </div>
-  )};
-  
+  )
+};
+
 export default Home;
