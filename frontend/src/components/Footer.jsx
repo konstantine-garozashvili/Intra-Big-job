4ccc0bb0308a3a2860fa5fcee1b7608f7f082976
camara_aicha <aicha.ca@outlook.fr>
2025-03-06 10:35:12 +0100

    Re-structure du fichier

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="relative mt-16 bg-[#00284f] rounded-md border border-[#DEC2E8] mb-[6mm] mx-[3mm]">
+    <div className="relative  bg-[#00284f] rounded-md ">
       <div className="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
           {/* Logo & Infos */}
@@ -8,13 +8,13 @@ const Footer = () => {
             <a href="/" aria-label="Go home" title="Company" className="inline-flex items-center">
               <img className="w-24 h-auto rounded-sm" src=" " alt="Logo" />
               <span className="ml-2 text-xl font-bold tracking-wide text-white uppercase">
-                Ninja - Centre de Formation
+                Big Project
               </span>
             </a>
             <div className="mt-4 lg:max-w-sm">
-              <p className="text-sm text-white">Ninja - Centre de Formation</p>
+              <p className="text-sm text-white">Description</p>
               <p className="mt-4 text-sm font-bold text-white">Nos coordonnées :</p>
-              <p className="mt-1 text-sm text-white">Email : ninja@laplateforme.io</p>
+              <p className="mt-1 text-sm text-white">Email : Bigproject@laplateforme.io</p>
               <p className="mt-1 text-sm text-white">Adresse : 30 Place Jules Guesdes, 13003 MARSEILLE</p>
             </div>
           </div>
@@ -24,25 +24,38 @@ const Footer = () => {
             {[
               {
                 title: "Lien rapide",
-                links: ["Contact", "Forum", "FAQ","CGU"],
+                links: [
+                  { name: "Accueil", href: "/home" },
+                  { name: "Contact", href: "/contact" },
+                  { name: "Forum", href: "/forum" },
+                  { name: "FAQ", href: "/faq" },
+                  
+                ],
               },
               {
-                title: "Titre?",
-                links: ["À propos de nous", "Nos formations", "Mentions légales", "CGU"],
+                title: "Nos Services",
+                links: [
+                  { name: "À propos de nous", href: "/about" },
+                  { name: "Nos formations", href: "/formations" },
+                  { name: "Mentions légales", href: "/mentions-legales" },
+                  { name: "CGU", href: "/cgu" },
+                ],
               },
               {
-                title: "Titre?",
-                links: ["Nos partenaires", "Devenir partenaires"],
+                title: "Partenariats",
+                links: [
+                  { name: "Nos partenaires", href: "/partenaires" },
+                  { name: "Devenir partenaire", href: "/devenir-partenaire" },
+                ],
               },
-              
             ].map((section, index) => (
-              <div key={index} className={`px-4 ${index !== 3 ? "border-r border-white/50" : ""}`}>
+              <div key={index} className={`px-4 ${index !== 2 ? "border-r border-white/50" : ""}`}>
                 <p className="font-semibold underline decoration-1 underline-offset-4">{section.title}</p>
                 <ul className="mt-2 space-y-2">
                   {section.links.map((link, idx) => (
                     <li key={idx}>
-                      <a href="/" className="transition-colors duration-300 hover:text-teal-400">
-                        {link}
+                      <a href={link.href} className="transition-colors duration-300 hover:text-[#528eb2]">
+                        {link.name}
                       </a>
                     </li>
                   ))}
@@ -55,7 +68,7 @@ const Footer = () => {
         {/* Footer Bas */}
         <div className="flex flex-col justify-between pt-5 pb-10 border-t border-white/50 sm:flex-row">
           <p className="text-sm text-white">
-            © Copyright 2025 - BigProject - Tous droits réservés.
+            © Copyright 2025 - Big Project - Tous droits réservés.
           </p>
           {/* Icônes Réseaux Sociaux (à compléter) */}
           <div className="flex items-center mt-4 space-x-4 sm:mt-0 text-white">
