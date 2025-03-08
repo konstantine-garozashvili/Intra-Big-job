13a8c6f2884a765ae1dbc8728b60fd2d34e88ba6
camara_aicha <aicha.ca@outlook.fr>
2025-03-08 14:30:36 +0100

    ajouts du tiroir plannings et aide

--- frontend/src/components/MenuBurger.jsx
+++ frontend/src/components/MenuBurger.jsx
@@ -2,7 +2,7 @@ import React, { memo, useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { authService } from '../lib/services/authService';
 import { Link } from 'react-router-dom';
-import {User, UserPlus, Shield , Users, GraduationCap,Calendar,MessageCircle,FileText,BookOpen,CalendarDays,ScrollText,HelpCircle, Bell, PiggyBank,Camera,Handshake,Headphones,School,LayoutDashboard} from 'lucide-react';
+import {User, UserPlus, Shield , Users, GraduationCap,Calendar,MessageCircle,FileText,BookOpen,CalendarDays,ScrollText,HelpCircle, Bell, PiggyBank,Camera,Handshake,Headphones,School,LayoutDashboard,Briefcase, Share2,Clipboard} from 'lucide-react';
 
 
 const MenuBurger = memo(() => {
@@ -89,6 +89,29 @@ const MenuBurger = memo(() => {
       links: [
         { name: 'Gestion des utilisateurs', to: '/admin/utilisateurs' },
         { name: 'Paramètres système', to: '/admin/parametres' },
+        { name: 'Les logs', to: '/admin/logs' },
+      ],
+    },
+    {
+      key: 'plannings',
+      label: 'Plannings',
+      icon: <Calendar className="mr-2" />,
+      roles: ['ROLE_SUPERADMIN', 'ROLE_ADMIN', 'ROLE_TEACHER','ROLE_HR','ROLE_RECRUITER','ROLE_STUDENT'],
+      links: [
+        { name: 'Evènement', to: '/plannings/évènements' },
+        
+      ],
+    },
+    {
+      key: 'aide',
+      label: 'Besoin d’aide ?',
+      icon: <Calendar className="mr-2" />,
+      roles: ['ROLE_SUPERADMIN', 'ROLE_ADMIN', 'ROLE_TEACHER','ROLE_HR','ROLE_RECRUITER','ROLE_STUDENT','ROLE_GUEST'],
+      links: [
+        { name: 'FAQ', to: '/aide/faq' },
+        { name: 'Forum', to: '/aide/forum' },
+        { name: 'Supports', to: '/aide/faq' },
+        
       ],
     },
     
@@ -109,7 +132,7 @@ const MenuBurger = memo(() => {
             animate={{ x: 0 }}
             exit={{ x: '-100%' }}
             transition={{ duration: 0.3 }}
-            className="fixed top-0 left-0 w-64 h-full bg-[#00284f] text-white shadow-lg z-50"
+            className="fixed top-0 left-0 w-70 h-full bg-[#00284f] text-white shadow-lg z-50"
           >
             <div className="flex flex-col h-full">
               <div className="flex items-center p-4 border-b border-blue-700">
@@ -129,6 +152,12 @@ const MenuBurger = memo(() => {
                     <li className="flex items-center px-4 py-2 hover:bg-blue-800">
                       <LayoutDashboard className="w-5 h-5 mr-2" />
                       <Link to="/dashboard">Tableau de bord</Link>
+                    </li>
+                  )}
+                  {(userRole === 'ROLE_STUDENT' || userRole === 'ROLE_TEACHER'|| userRole === 'ROLE_ADMIN'|| userRole === 'ROLE_SUPERADMIN') &&(
+                    <li className="flex items-center px-4 py-2 hover:bg-blue-800">
+                      <Bell className="w-5 h-5 mr-2" />
+                      <Link to="/notifications">Notifications</Link>
                     </li>
                   )}
                   {menuItems.map(({ key, icon, label, roles, links }) =>
@@ -164,37 +193,20 @@ const MenuBurger = memo(() => {
 
                   {(userRole === 'ROLE_STUDENT' || userRole === 'ROLE_TEACHER'|| userRole === 'ROLE_ADMIN'|| userRole === 'ROLE_SUPERADMIN') &&(
                     <li className="flex items-center px-4 py-2 hover:bg-blue-800">
-                      <Calendar className="w-5 h-5 mr-2" />
-                      <Link to="/plannings">Mon planning</Link>
-                    </li>
-                  )}
-                  {(userRole === 'ROLE_STUDENT' || userRole === 'ROLE_TEACHER'|| userRole === 'ROLE_ADMIN'|| userRole === 'ROLE_SUPERADMIN') &&(
-                    <li className="flex items-center px-4 py-2 hover:bg-blue-800">
                       <MessageCircle className="w-5 h-5 mr-2" />
                       <Link to="/messagerie">Ma Messagerie</Link>
                     </li>
                   )}
-                  {(userRole === 'ROLE_ADMIN'|| userRole === 'ROLE_SUPERADMIN' || userRole === 'ROLE_RECRUITER') &&(
-                    <li className="flex items-center px-4 py-2 hover:bg-blue-800">
-                      <FileText className="w-5 h-5 mr-2" />
-                      <Link to="/test_admission">Test d'admission</Link>
-                    </li>
-                  )}
 
-                  {(userRole === 'ROLE_STUDENT' || userRole === 'ROLE_TEACHER'|| userRole === 'ROLE_ADMIN'|| userRole === 'ROLE_SUPERADMIN') &&(
-                    <li className="flex items-center px-4 py-2 hover:bg-blue-800">
-                      <CalendarDays className="w-5 h-5 mr-2" />
-                      <Link to="/evenements">Evènements</Link>
-                    </li>
-                  )}
+                  
                   {(userRole === 'ROLE_STUDENT' || userRole === 'ROLE_TEACHER') && (
                     <>
                       <li className="flex items-center px-4 py-2 hover:bg-blue-800">
-                        <GraduationCap className="w-5 h-5 mr-2" />
+                        <Clipboard className="w-5 h-5 mr-2" />
                         <Link to="/cours">Mes Cours</Link>
                       </li>
                       <li className="flex items-center px-4 py-2 hover:bg-blue-800">
-                        <GraduationCap className="w-5 h-5 mr-2" />
+                        <Briefcase className="w-5 h-5 mr-2" />
                         <Link to="/projet">Mes Projets</Link>
                       </li>
                     </>
@@ -205,38 +217,20 @@ const MenuBurger = memo(() => {
                       <Link to="/centres_formations">Centres de formations</Link>
                     </li>
                   )}
-                  {userRole === 'ROLE_SUPERADMIN' && (
-                    <li className="flex items-center px-4 py-2 hover:bg-blue-800">
-                      <ScrollText className="w-5 h-5 mr-2" />
-                      <Link to="/logs">Les Logs</Link>
-                    </li>
-                  )}
+                  
                   {(userRole === 'ROLE_HR' || userRole === 'ROLE_RECRUITER') && (
                     <li className="flex items-center px-4 py-2 hover:bg-blue-800">
-                      <GraduationCap className="w-5 h-5 mr-2" />
+                      <Share2 className="w-5 h-5 mr-2" />
                       <Link to="/candidatures">Candidatures</Link>
                     </li>
                   )}
                   
-                  <li className="flex items-center px-4 py-2 hover:bg-blue-800">
-                    <HelpCircle className="w-5 h-5 mr-2" />
-                    <Link to="/faq">FAQ</Link>
-                  </li>
                   
-                  <li className="flex items-center px-4 py-2 hover:bg-blue-800">
-                    <GraduationCap className="w-5 h-5 mr-2" />
-                    <Link to="/forum">Forum</Link>
-                  </li>
                   <li className="flex items-center px-4 py-2 hover:bg-blue-800">
                   <BookOpen className="w-5 h-5 mr-2" />
                     <Link to="/formations">Formations</Link>
                   </li>
-                  {(userRole === 'ROLE_STUDENT' || userRole === 'ROLE_TEACHER'|| userRole === 'ROLE_ADMIN'|| userRole === 'ROLE_SUPERADMIN') &&(
-                    <li className="flex items-center px-4 py-2 hover:bg-blue-800">
-                      <Bell className="w-5 h-5 mr-2" />
-                      <Link to="/notifications">Notifications</Link>
-                    </li>
-                  )}
+                  
                   {(userRole === 'ROLE_STUDENT' || userRole === 'ROLE_TEACHER'|| userRole === 'ROLE_ADMIN'|| userRole === 'ROLE_SUPERADMIN') &&(
                     <li className="flex items-center px-4 py-2 hover:bg-blue-800">
                       <PiggyBank className="w-5 h-5 mr-2" />
@@ -259,10 +253,6 @@ const MenuBurger = memo(() => {
               </div>
 
               <div className="p-4 border-t border-blue-700">
-                <button className="flex items-center w-full text-left hover:bg-blue-800 px-4 py-2 mt-2">
-                  <Headphones className="w-5 h-5 mr-2" />
-                  <Link to="/support">Support</Link>
-                </button>
                 <button className="flex items-center w-full text-left hover:bg-blue-800 px-4 py-2 mt-2">
                   Déconnexion
                 </button>
