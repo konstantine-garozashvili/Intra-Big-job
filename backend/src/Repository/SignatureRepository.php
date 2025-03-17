855adc22b88549cac38ab934c655d4bb6c1595ba
yanis-bennadji <yanis-bennadji@laplateforme.io>
2025-03-17 10:26:46 +0100

    added a missing query

--- backend/src/Repository/SignatureRepository.php
+++ backend/src/Repository/SignatureRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Signature;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
