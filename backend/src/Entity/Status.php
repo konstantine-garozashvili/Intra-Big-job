0a12a3c632ea737b40c72cd7f82ae86b2a277955
RyanLadmia <ryan.ladmia@laplateforme.io>
2025-03-17 14:50:14 +0100

    Creation de l'entité status

---
+++ backend/src/Entity/Status.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StatusRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StatusRepository::class)]
+class Status
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $user_id = null;
+
+    #[ORM\Column]
+    private ?int $status_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getStatusId(): ?int
+    {
+        return $this->status_id;
+    }
+
+    public function setStatusId(int $status_id): static
+    {
+        $this->status_id = $status_id;
+
+        return $this;
+    }
+}
