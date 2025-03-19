f236774945f598bc9d3832ba03dce46f58d38ba1
konstantine-garozashvili <konstantine.garozashvili@laplateforme.io>
2025-03-19 13:54:52 +0100

    update service and transations

--- frontend/src/components/PageTransition.jsx
+++ frontend/src/components/PageTransition.jsx
@@ -45,25 +45,3 @@ const PageTransition = memo(({ children }) => {
   );
 });
 
-// Ajout d'un nom d'affichage pour les outils de développement
-PageTransition.displayName = 'PageTransition';
-
-export default PageTransition; 
-import { memo } from 'react';
-
-// Utilisation de React.memo pour éviter les rendus inutiles
-const PageTransition = memo(({ children }) => {
-  // Utilisation de classes CSS pour les animations au lieu de Framer Motion
-  return (
-    <div
-      className="fade-in-up hw-accelerated"
-    >
-      {children}
-    </div>
-  );
-});
-
-// Ajout d'un nom d'affichage pour les outils de développement
-PageTransition.displayName = 'PageTransition';
-
-export default PageTransition; 
