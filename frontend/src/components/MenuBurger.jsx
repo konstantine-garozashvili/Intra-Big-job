36d63a0dc7b6a6e5da3ae625435b88a00b95d750
ArthurDesc <arthur.descourvieres@gmail.com>
2025-03-19 09:45:27 +0100

    feat: add roles for guest access in MenuBurger component

--- frontend/src/components/MenuBurger.jsx
+++ frontend/src/components/MenuBurger.jsx
@@ -425,6 +425,7 @@ const MenuBurger = memo(() => {
       key: 'nous_rejoindre',
       label: 'Nous rejoindre',
       icon: <Users className="w-5 h-5 mr-2 text-[#528eb2]" />,
+      roles: [ROLES.GUEST],
       links: [
         { name: 'Envoyer une candidature', to: '/nous-rejoindre/candidature' },
         { name: 'Processus de recrutement', to: '/nous-rejoindre/recrutement' },
