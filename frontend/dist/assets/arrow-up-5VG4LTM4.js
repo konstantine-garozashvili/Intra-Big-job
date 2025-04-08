6aa2d6daf565e43b44a114e90c8671c887dbcedd
konstantine-garozashvili <konstantine.garozashvili@laplateforme.io>
2025-04-08 14:33:52 +0200

    Add production frontend build

---
+++ frontend/dist/assets/arrow-up-5VG4LTM4.js
@@ -0,0 +1,7 @@
+import{s as a}from"./index-Bd05LC-l.js";
+/**
+ * @license lucide-react v0.476.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=a("ArrowDown",[["path",{d:"M12 5v14",key:"s699le"}],["path",{d:"m19 12-7 7-7-7",key:"1idqje"}]]),r=a("ArrowUp",[["path",{d:"m5 12 7-7 7 7",key:"hav0vg"}],["path",{d:"M12 19V5",key:"x0mq9r"}]]);export{r as A,e as a};
---
+++ frontend/dist/assets/chevron-up-Db7ZLIiO.js
@@ -0,0 +1,7 @@
+import{s as o}from"./index-Bd05LC-l.js";
+/**
+ * @license lucide-react v0.476.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=o("ChevronUp",[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]]);export{s as C};
