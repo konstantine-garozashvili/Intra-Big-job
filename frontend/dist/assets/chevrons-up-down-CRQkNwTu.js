af72c9b9ea4dd48c326fe95ed4ea47aefd26115b
ArthurDesc <arthur.descourvieres@gmail.com>
2025-04-10 14:58:57 +0200

    Removed production files

--- frontend/dist/assets/chevrons-up-down-CRQkNwTu.js
+++
@@ -1,7 +0,0 @@
-import{s}from"./index-Bd05LC-l.js";
-/**
- * @license lucide-react v0.476.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=s("ChevronsUpDown",[["path",{d:"m7 15 5 5 5-5",key:"1hf1tw"}],["path",{d:"m7 9 5-5 5 5",key:"sgt6xg"}]]);export{t as C};
--- frontend/dist/assets/progress-DdD1i5d6.js
+++
@@ -1,7 +0,0 @@
-import{s}from"./index-Bd05LC-l.js";import{j as r}from"./ui-B9MhWaVJ.js";import{R as e}from"./router-D7BdrHDj.js";
-/**
- * @license lucide-react v0.476.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const l=s("TrendingUp",[["polyline",{points:"22 7 13.5 15.5 8.5 10.5 2 17",key:"126l90"}],["polyline",{points:"16 7 22 7 22 13",key:"kwv8wd"}]]);e.forwardRef((({className:s,value:e=0,...l},a)=>r.jsx("div",{ref:a,className:`relative h-4 w-full overflow-hidden rounded-full bg-secondary ${s||""}`,...l,children:r.jsx("div",{className:"h-full w-full flex-1 bg-primary transition-all",style:{transform:`translateX(-${100-(e||0)}%)`,transition:"transform 0.2s ease-in-out"}})}))).displayName="Progress";export{l as T};
