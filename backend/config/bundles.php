051c17925c10733d5270a9245021efab5a22556a
konstantine-garozashvili <konstantine.garozashvili@laplateforme.io>
2025-04-11 09:33:56 +0200

    Update bundles.php

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -13,7 +13,8 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    // Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true], // Temporarily disabled for testing
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    // Add aws/aws-sdk-php-symfony if it exists in the original file
 ];
