e71a7fefc03b0cc4c5029a7dafcc6c19ff4f8ec2
ArthurDesc <arthur.descourvieres@gmail.com>
2025-04-15 16:23:52 +0200

    npm run build

---
+++ frontend/dist/assets/chevrons-up-down-B7IaldaF.js
@@ -0,0 +1,7 @@
+import{Y as t}from"./index-DebaQ5DR.js";
+/**
+ * @license lucide-react v0.476.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=t("ChevronsUpDown",[["path",{d:"m7 15 5 5 5-5",key:"1hf1tw"}],["path",{d:"m7 9 5-5 5 5",key:"sgt6xg"}]]);export{o as C};
