b87454e2456d992a622a3160027343424b378171
ArthurDesc <arthur.descourvieres@gmail.com>
2025-05-09 21:41:49 +0200

    refactor(assets): rename asset files and update import paths

---
+++ frontend/dist/assets/circle-x-BaUdcrHg.js
@@ -0,0 +1,7 @@
+import{y as e}from"./index-2R14kkJt.js";
+/**
+ * @license lucide-react v0.476.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("CircleX",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]]);export{r as C};
