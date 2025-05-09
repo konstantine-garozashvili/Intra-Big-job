990c4efabf2e5a5f6dd4aa8fd60d8e5a11aa414f
ArthurDesc <arthur.descourvieres@gmail.com>
2025-05-09 21:31:59 +0200

    refactor(assets): rename multiple asset files and update import paths

--- frontend/src/lib/services/firebase.js
+++ frontend/src/lib/services/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAXPOWUzhGZNFjuVXX-bRZYyOqVRUL-vwQ",
   authDomain: "bigproject-d6daf.firebaseapp.com",
   projectId: "bigproject-d6daf",
-  storageBucket: "bigproject-d6daf.firebasestorage.app",
+  storageBucket: "bigproject-d6daf.appspot.com",
   messagingSenderId: "540438748170",
   appId: "1:540438748170:web:416d4e38ac4a3be11513f3",
   measurementId: "G-MVPMBS32W2"
