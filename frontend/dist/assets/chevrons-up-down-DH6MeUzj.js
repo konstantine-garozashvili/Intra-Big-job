70c507a2b3fa701b9f2dcf0717660462d076cf95
ArthurDesc <arthur.descourvieres@gmail.com>
2025-05-11 11:27:26 +0200

    refactor(assets): rename asset files and update import paths for consistency

---
+++ frontend/dist/assets/chevrons-up-down-DH6MeUzj.js
@@ -0,0 +1,7 @@
+import{y as t}from"./index-XAN29Is_.js";
+/**
+ * @license lucide-react v0.476.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=t("ChevronsUpDown",[["path",{d:"m7 15 5 5 5-5",key:"1hf1tw"}],["path",{d:"m7 9 5-5 5 5",key:"sgt6xg"}]]);export{o as C};
